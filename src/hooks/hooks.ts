71fc7f3a26016459049ac9b6e604e35e99f0ef28
Jayant Kale <jayant.kale@polestar.com>
2023-10-18 00:16:51 +0200

    first commit

---
+++ src/hooks/hooks.ts
@@ -0,0 +1,27 @@
+import { BeforeAll, AfterAll, Before, After } from '@cucumber/cucumber';
+import { Browser, chromium, Page } from '@playwright/test';
+import { pageFixture } from './pageFixture';
+
+let browser: Browser;
+let page: Page;
+
+BeforeAll(async () => {
+  console.log('Launch Browser using Playwright and Chromium browser, performed once, before the start of all test scenarios.');
+  browser = await chromium.launch({headless: false});
+});
+
+AfterAll(async () => {
+  console.log('Closes Browser before the start of each scenario');
+  await browser.close();
+});
+
+Before(async () => {
+  console.log('Launch Browser, performed before each individual test scenario');
+  page = await browser.newPage();
+  pageFixture.page = page;
+});
+
+After(async () => {
+  console.log('Browser closed after each scenario');
+  await page.close();
+});
